5bab96808100dace22902193cfa70db56e6ce52f
Tamara Zdravkovic <tzdravkovic1999@gmail.com>
2024-03-26 15:24:45 +0100

    Completion of the task 2 modules

--- Cas 21/main2.js
+++ Cas 21/main2.js
@@ -1,3 +1,5 @@
+import mainProducts from "./productsService.js"
+
 class Product {
     constructor(name, category, price) {
         this.id = Math.floor(Math.random() * 1000)
@@ -6,3 +8,13 @@ class Product {
         this.price = Number(price) || 0
     }
 }
+
+const products = [
+    new Product("Laptop", "Electronics", 1700),
+    new Product("Dress", "Clothing", 35),
+    new Product("T-Shirt", "Clothing", 15),
+    new Product("Vacuum cleaner", "Electronics", 70),
+    new Product("Headphones", "Electronics", 60),
+  ];
+  
+  console.log(mainProducts.getTheCheapestProduct(products))
--- Cas 21/productsService.js
+++ Cas 21/productsService.js
@@ -1,8 +1,33 @@
-const getProductsByCategory = (products, category) => products.filter((product) => {product.category === category})
- 
+const getProductsByCategory = (products, category) =>
+  products.filter((product) => {
+    product.category === category;
+  });
 
-const getSumOfPrices = (sum) => 
-sum.reduce((accumulator, currentValue) => {return accumulator + currentValue}, 0)
+const getSumOfPrices = (products) =>
+  products.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.price;
+  }, 0);
 
+const getAveragePrice = (products) => {
+  let sum = getSumOfPrices(products);
+  return sum / products.length;
+};
 
-const getAveragePrice = (average) => 
+const getTheCheapestProduct = (products) => {
+  let price = Number.POSITIVE_INFINITY;
+  let cheapestProduct
+  products.forEach((product) => {
+    if (product.price < price) {
+      price = product.price;
+      cheapestProduct = product
+    }
+  });
+  return cheapestProduct;
+};
+
+export default{
+    getProductsByCategory,
+    getSumOfPrices,
+    getAveragePrice,
+    getTheCheapestProduct
+}
